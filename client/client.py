e495a45d396627b749637a9fe099b73a6ca25127
OlafurAO <olirokkar@gmail.com>
2024-02-17 23:25:07 +0100

    ?

--- client/client.py
+++ client/client.py
@@ -1,4 +1,8 @@
 import os
+import time
+import socket
+import selectors
+import types
 from enum import Enum
 
 from server.server import Server
@@ -11,7 +15,9 @@ class ClientState(Enum):
 class Client:
   def __init__(self) -> None:
     self.server = Server()
+    self.selector = selectors.DefaultSelector()
     self.menu = Menu()
+    self.client_socket = None
     self.client_state = ClientState.MENU
     self.join_server_fields = { 'IP_ADDRESS': None, 'PORT': None, 'PASSWORD': None }
     self.quit = False
@@ -20,15 +26,70 @@ class Client:
   def run(self):
     while not self.quit:
       self.update()
+      
     print('\033[0m')
 
   def update(self):
-    os.system('cls' if os.name == 'nt' else 'clear')
-    if ClientState.MENU:
-      self.menu.update_menu(self.server.get_host_info(), self.join_server_fields)    
+    if self.client_state == ClientState.MENU:
+      os.system('cls' if os.name == 'nt' else 'clear')
+      self.menu.update_menu(self.server.get_host_info(), self.join_server_fields)
+    elif self.client_state == ClientState.PLAY:
+      pass
+
+    #if self.client_socket is not None:
+      
+
     print('>$', end='')
     self.process_command(input())
 
   def process_command(self, command: str):
-    if ClientState.MENU:
-      self.menu.process_menu_commands(self.server, self.join_server_fields, command)
+    if self.client_state == ClientState.MENU:
+      self.menu.process_menu_commands(self.server, self.join_server_fields, self.join_server, command)
+    elif self.client_state == ClientState.PLAY:
+      self.send_server_message(command)
+      self.receive_server_message()
+
+  def join_server(self, server_fields):
+    time.sleep(1)
+    # TODO: replace with server_fields values
+    server_address = ('192.168.1.86', 58008)
+    max_retries = 5
+
+    for attempt in range(max_retries):
+      try:
+        print(f'\n[ATTEMPT {attempt}] CONNECTING TO SERVER...')
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.setblocking(False)
+        self.client_socket.connect_ex(server_address)
+
+        print(f'[ATTEMPT {attempt}] SUCCESSFULLY CONNECTED...\n')
+
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(outb=b'')  
+        self.selector.register(self.client_socket, events, data=data)
+
+        self.client_state = ClientState.PLAY
+        break
+      except ConnectionRefusedError:
+        time.sleep(1)
+
+  def send_server_message(self, command):
+    self.client_socket.sendall(command.encode())
+
+  def receive_server_message(self):
+    for key, mask in self.selector.select(timeout=86400):
+      if key.data:
+        client_socket = key.fileobj
+        if mask & selectors.EVENT_READ:
+          try:
+            recv_data = client_socket.recv(1024)
+            if recv_data:
+              print(f'RECEIVED MESSAGE FROM SERVER {recv_data.decode()}')
+            else:
+              # Server closed the connection
+              self.quit = True
+              break
+          except ConnectionResetError:
+            print('Connection with server reset unexpectedly.')
+            self.quit = True
+            break
--- menu/menu.py
+++ menu/menu.py
@@ -21,13 +21,13 @@ class Menu:
     elif self.client_state == MenuState.JOIN_SERVER:
       self.graphics.display_join_server_menu(server_fields)
 
-  def process_menu_commands(self, server, server_fields, command=str):
+  def process_menu_commands(self, server, server_fields, join_server, command=str):
     if self.client_state == MenuState.MAIN_MENU:
       self.handle_main_menu_command(command)
     elif self.client_state == MenuState.CREATE_SERVER:
-      self.handle_create_server_command(server, command)
+      self.handle_create_server_command(server, join_server, command)
     elif self.client_state == MenuState.JOIN_SERVER:
-      self.handle_join_server_command(server, server_fields, command)
+      self.handle_join_server_command(server_fields, join_server, command)
 
   def handle_main_menu_command(self, command: str):
     print(command)
@@ -38,17 +38,17 @@ class Menu:
     elif command == 'quit':
       self.quit = True
 
-  def handle_create_server_command(self, server, command: str):
+  def handle_create_server_command(self, server, join_server, command: str):
     if command is not None:
       subprocess.Popen(['python', 'server_launcher.py', command])
       host_info = server.get_host_info()
-      server.join_server({ 'IP_ADDRESS': host_info['ip_address'], 'PORT': 58008, 'PASSWORD': command })
+      join_server({ 'IP_ADDRESS': host_info['ip_address'], 'PORT': 58008, 'PASSWORD': command })
 
-  def handle_join_server_command(self, server, server_fields, command: str):
+  def handle_join_server_command(self, server_fields, join_server, command: str):
     if server_fields['IP_ADDRESS'] is None:
       server_fields['IP_ADDRESS'] = command
     elif server_fields['PORT'] is None:
       server_fields['PORT'] = command
     elif server_fields['PASSWORD'] is None:
       server_fields['PASSWORD'] = command
-      server.join_server(server_fields)
+      join_server(server_fields)
